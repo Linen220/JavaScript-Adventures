657b63e2fc15ecb97fbfe41128cf63f4170007af
Linen220 <fernandorondon2009@hotmail.com>
2023-10-22 10:42:55 -0500

    adding to-do-list-app css styles

--- To-Do-List-App/assets/js/script.js
+++ To-Do-List-App/assets/js/script.js
@@ -1,1 +1,18 @@
-git 
+var maxProfit = function(prices) {
+  
+    let maxProfit = 0;
+    let minBuy = prices[0];
+
+    for (let i = 1; i < prices.length; i++) {
+        minBuy = Math.min(minBuy, prices[i])
+        maxProfit = Math.max(maxProfit, prices[i] - minBuy );
+    }
+
+    return maxProfit;
+};
+
+
+// const prices = [7,1,5,3,6,4];
+const prices = [2,4,1];
+
+console.log(maxProfit(prices));
