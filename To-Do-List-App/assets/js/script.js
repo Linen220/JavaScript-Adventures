f6d817b52279f72243c94174316173ad481b8d1f
Linen220 <fernandorondon2009@hotmail.com>
2023-10-25 00:04:07 -0500

    adding to-do-list-app responsive

--- To-Do-List-App/assets/js/script.js
+++ To-Do-List-App/assets/js/script.js
@@ -1,31 +1,37 @@
 const listTask = document.querySelector('#list-task');
 const inputBox = document.querySelector('#input-box');
+const btnAdd = document.querySelector('#btn-add');
 
 
-function addTask() {
+btnAdd.addEventListener('click', () => {
     if ( inputBox.value === '' ) {
-        alert('Enter Some Data')
+        alert('Ingrese una tarea')
     } else {
         let task = document.createElement('li');
         task.innerText = inputBox.value;
         listTask.append(task);
 
+        //* Insertamos la cruz para eliminar la tarea.
         let removeIcon = document.createElement('span');
         removeIcon.innerHTML = '\u00d7'; //? \u00d7 -> es el simbolo de la cruz.
-
         task.append(removeIcon);
     }
     inputBox.value = '';
     saveTask();
-}
+});
 
 listTask.addEventListener('click', ( e ) => {
+    //* e -> es el evento que se pasa a la función de controlador de eventos.     
+    //* e.target -> se refiere al elemento que desencadenó el evento.
+    //* e.target.parentElement -> accede al elemento padre del elemento que desencadenó el evento.
+    // console.log(e.target)
+    // console.log(e.target.parentElement)
+    
     if ( e.target.tagName === 'LI' ) {
         e.target.classList.toggle('checked');
         saveTask();
-    } 
-    else if ( e.target.tagName === 'SPAN' ) {
-        e.target.classList.remove();
+    } else if ( e.target.tagName === 'SPAN' ) {
+        e.target.parentElement.remove();
         saveTask();
     }
 });
