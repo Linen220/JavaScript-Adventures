a5bfcaf214773ae15601a1683a07792982f66f77
Linen220 <fernandorondon2009@hotmail.com>
2023-10-24 23:31:29 -0500

    adding to-do-list-app script.js

--- To-Do-List-App/assets/js/script.js
+++ To-Do-List-App/assets/js/script.js
@@ -9,18 +9,31 @@ function addTask() {
         let task = document.createElement('li');
         task.innerText = inputBox.value;
         listTask.append(task);
+
+        let removeIcon = document.createElement('span');
+        removeIcon.innerHTML = '\u00d7'; //? \u00d7 -> es el simbolo de la cruz.
+
+        task.append(removeIcon);
     }
     inputBox.value = '';
     saveTask();
 }
 
-listTask.addEventListener('click', () => {
-    
+listTask.addEventListener('click', ( e ) => {
+    if ( e.target.tagName === 'LI' ) {
+        e.target.classList.toggle('checked');
+        saveTask();
+    } 
+    else if ( e.target.tagName === 'SPAN' ) {
+        e.target.classList.remove();
+        saveTask();
+    }
 });
 
 
 
 //* Local Storage - útil para mantener la información despues de actualizar el navegador
+//* Guarda la información actual en el localstorage
 function saveTask() {
     localStorage.setItem('data', listTask.innerHTML);
 }
