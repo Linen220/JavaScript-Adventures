55ed3f30720e92b2dd191f07ef4633997c71043d
Linen220 <fernandorondon2009@hotmail.com>
2023-11-16 11:16:14 -0500

    adding js final

--- Random-Password-Generator/main.js
+++ Random-Password-Generator/main.js
@@ -1,10 +1,13 @@
 const inputSlider = document.getElementById('inputSlider');
 const sliderValue = document.getElementById('sliderValue');
 const passBox = document.getElementById('passBox');
-const lowecaseElement = document.getElementById('lowecase');
+const lowercaseElement = document.getElementById('lowercase');
+const uppercaseElement = document.getElementById('uppercase');
 const numberElement = document.getElementById('numbers');
 const symbolsElement = document.getElementById('symbols');
 const generateBtn = document.getElementById('getBtn');
+
+const copyIcon = document.getElementById('copyIcon');
 
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -22,6 +25,38 @@ generateBtn.addEventListener('click', () => {
 });
 
 function generatePaswword() {
-    const length = inputSlider.value
+    const length = inputSlider.value;
+    let characters = '';
+    let password = '';
+
+    characters += lowercaseElement.checked ? lowercaseLetters : '';
+    characters += uppercaseElement.checked ? uppercaseLetters : '';
+    characters += numberElement.checked ? numbers : '';
+    characters += symbolsElement.checked ? symbols : '';
+    
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    
+    return password;
 }
+
+//* Copiar el password al portapapeles.
+copyIcon.addEventListener('click', () => {
+    if ( passBox.value !== '' || passBox.value.length >= 10 ) {
+
+        //* Esta línea copia el password al portapapeles.
+        navigator.clipboard.writeText(passBox.value);
+
+        //* Aqui se cambia el icono de guardado a un 'check'.
+        //* El nombre anterior es 'content_copy' pero al cambiarlo a 'check'
+        //* se cambia el icono.
+        copyIcon.innerText = 'check';
+
+        //* El icono volvera a su icono original despues de 3 segundos.
+        setTimeout(() => {
+           copyIcon.innerHTML = 'content_copy'; 
+        },3000 );
+    }
+});
 
